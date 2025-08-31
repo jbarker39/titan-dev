0eee651995ccbf99bb048673db3894e1ea52f2d7
Joe Barker <jbarker39@gmail.com>
2025-08-31 12:20:59 -0700

    changed print statement

--- argtest.py
+++ argtest.py
@@ -2,4 +2,4 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", help="display a square of a given number",type=int)
 args = parser.parse_args()
-print (args.square**2)
+print ("square of {} = {}".format(args.square,args.square**2))
